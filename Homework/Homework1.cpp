16c01722fa797eeb4fb6224726317714295ba867
Der Meister <morgenstern92@inbox.ru>
2021-05-30 10:59:27 +0600

    Добавлен летающий шарик

--- Homework/Homework1.cpp
+++ Homework/Homework1.cpp
@@ -5,17 +5,17 @@ const int WindowWidth  = 1400,
           WindowHeight = 700,
           SleepTime    = 1;
 
-void MadamGo (int x, int y);
+void MadamAndBall (int x, int y, int x1, int y1, int vx, int vy, int dt);
 
 int main ()
     {
     txCreateWindow (WindowWidth, WindowHeight);
     txClear ();
 
-    MadamGo (100, 600);
+    MadamAndBall (100, 600, 100, 100, 5, 3, 2);
     }
 
-void MadamGo (int x, int y)
+void MadamAndBall (int x, int y, int x1, int y1, int vx, int vy, int dt)
     {
     txBegin ();
     while (! txGetAsyncKeyState (VK_ESCAPE))
@@ -25,6 +25,34 @@ void MadamGo (int x, int y)
         MnogoTravi (480, 0);
         ZadniiPlan (40);
 
+        txSetColor (RGB (x, x % 250, 0), 3);
+        txSetFillColor (RGB (y, y % 250, 0));
+
+        txCircle (x1, y1, 20);
+        x1 += vx * dt;
+        y1 += vy * dt;
+
+        if (x1 > WindowWidth)
+            {
+            vx = -vx;
+            x1 = x1 - 2 * (x1 - WindowWidth);
+            }
+        if (x1 < 0)
+            {
+            vx = -vx;
+            x1 = x1 - 2 * x1;
+            }
+        if (y1 > WindowHeight)
+            {
+            vy = -vy;
+            y1 = y1 - 2 * (y1 - WindowHeight);
+            }
+        if (y1 < 0)
+            {
+            vy = -vy;
+            y1 = y1 - 2 * y1;
+            }
+
         if (x > WindowWidth - 20)   x = WindowWidth - 20;
         if (x < 20)                 x = 20;
         if (y > WindowHeight - 100) y = WindowHeight - 100;
@@ -33,7 +61,7 @@ void MadamGo (int x, int y)
         if ((! txGetAsyncKeyState (VK_DOWN )) && (! txGetAsyncKeyState (VK_UP  )) &&
             (! txGetAsyncKeyState (VK_RIGHT)) && (! txGetAsyncKeyState (VK_LEFT)))
             {
-            MadamDraw (x, y, 1, 1, 2, 1, 1, 0, 0, 0 + ((x + y) / 10) % 2, 1 - ((x + y) / 10) % 2, 0, 0, 1);
+            MadamDraw (x, y, 1, 1, 2, 1, 1, 0, 0, 1, 1, 0, 0, 1);
             }
 
         if (txGetAsyncKeyState (VK_RIGHT))

c2cebe9a1a36e4f4299db017431518c1f6b52062
Der Meister <82102202+Eisenmann-92@users.noreply.github.com>
2021-07-08 22:10:00 +0600

    Main menu

---
+++ MyFirstGame.cpp
@@ -0,0 +1,332 @@
+#include "TXLib.h"
+
+const int WindowWidth  = 1280,
+          WindowHeight = 720,
+          SleepTime    = 1;
+
+const HDC MainMenu = txLoadImage ("Pictures/Main_menu.bmp");
+const HDC Buttons = txLoadImage ("Pictures/Buttons.bmp");
+const HDC Score  = txLoadImage ("Pictures/Background&Score.bmp");
+const HDC Background  = txLoadImage ("Pictures/Background.bmp");
+const HDC BackToMenu  = txLoadImage ("Pictures/BackToMenu.bmp");
+const HDC Cat = txLoadImage ("Pictures/Cat.bmp");
+
+void MenuButtons (double CenterX, double CenterY, double ButtonsX, double ButtonsY, double MinXButtons,
+                  double MaxXButtons, double MinYNewGame, double MaxYNewGame, double MaxYControl,
+                  double MaxYExitGame);
+void Menu ();
+struct Ball
+    {int x,  y,
+         x1, y1,
+         vx, vy,
+         dt, r,
+         k;
+
+    void MoveBall ();
+    void Rectangle ();
+    void Score ();
+    };
+
+void GameOverText ();
+void MiniGame ();
+
+int main ()
+    {
+    txCreateWindow (WindowWidth, WindowHeight);
+    txClear ();
+
+    Menu ();
+    return 0;
+    }
+
+void MenuButtons (double CenterX, double CenterY, double ButtonsX, double ButtonsY, double MinXButtons,
+           double MaxXButtons, double MinYNewGame, double MaxYNewGame, double MaxYControl,
+           double MaxYExitGame)
+    {
+    txPlaySound ("Audio/MainMenu/Sound.wav");
+    int t = 0;
+    int exit = 0;
+    txBegin ();
+    while (exit < 1)
+        {
+        txClear ();
+        txBitBlt (txDC (), 0, 0, 0, 0, MainMenu, 0, 0);
+
+        int CatX = txGetExtentX (Cat)/4,
+            CatY = txGetExtentY (Cat)/8;
+
+        if (MinXButtons <= txMouseX () && txMouseX () <= MaxXButtons &&
+            MinYNewGame <= txMouseY () && txMouseY () <= MaxYNewGame)
+            {
+            txTransparentBlt (txDC (), CenterX - ButtonsX/2, CenterY - ButtonsY*1.5, ButtonsX, ButtonsY,
+                              Buttons, ButtonsX, 0, RGB (250, 249, 244));
+            if (txMouseButtons () == 1)
+                {
+                    MiniGame ();
+                }
+            }
+
+        else txTransparentBlt (txDC (), CenterX - ButtonsX/2, CenterY - ButtonsY*1.5, ButtonsX, ButtonsY,
+                               Buttons, 0, 0, RGB (250, 249, 244));
+
+        if (MinXButtons <= txMouseX () && txMouseX () <= MaxXButtons &&
+            MaxYNewGame <= txMouseY () && txMouseY () <= MaxYControl)
+            {
+            txTransparentBlt (txDC (), CenterX - ButtonsX/2, CenterY - ButtonsY/2, ButtonsX, ButtonsY,
+                              Buttons, ButtonsX, ButtonsY, RGB (250, 249, 244));
+            if (txMouseButtons () == 1)
+                {
+                while (! txGetAsyncKeyState (VK_ESCAPE))
+                    {
+                    txSetFillColor (TX_WHITE);
+                    txClear ();
+                    }
+                }
+            }
+
+        else txTransparentBlt (txDC (), CenterX - ButtonsX/2, CenterY - ButtonsY/2, ButtonsX, ButtonsY,
+                               Buttons, 0, ButtonsY, RGB (250, 249, 244));
+
+        if (MinXButtons <= txMouseX () && txMouseX () <= MaxXButtons &&
+            MaxYControl <= txMouseY () && txMouseY () <= MaxYExitGame)
+            {
+            txTransparentBlt (txDC (), CenterX - ButtonsX/2, CenterY + ButtonsY/2, ButtonsX, ButtonsY,
+                              Buttons, ButtonsX, ButtonsY*2, RGB (250, 249, 244));
+            if (txMouseButtons () == 1)
+                {
+                exit = 1;
+                }
+            }
+
+        else txTransparentBlt (txDC (), CenterX - ButtonsX/2, CenterY + ButtonsY/2, ButtonsX, ButtonsY,
+                               Buttons, 0, ButtonsY*2, RGB (250, 249, 244));
+
+        if (t <= 149)
+            txTransparentBlt (txDC (), 0 + t, 540, CatX, CatY, Cat,
+                              CatX * (((t / 10) % 2) + 2), CatY,     RGB (255, 255, 255));
+
+        if (149 < t && t <= 189)
+            txTransparentBlt (txDC (), 149,   540, CatX, CatY, Cat, CatX * (((t - 150) / 10) % 4),
+                              CatY * 6, RGB (255, 255, 255));
+
+        if (189 < t && t <= 199)
+            txTransparentBlt (txDC (), 149,   540, CatX, CatY, Cat, CatX * 3,
+                              CatY * 6, RGB (255, 255, 255));
+
+        if (199 < t && t <= 399)
+            txTransparentBlt (txDC (), 149,   540, CatX, CatY, Cat, CatX * ((((t - 200) / 20) % 2) + 2),
+                              CatY * 6, RGB (255, 255, 255));
+
+        if (399 < t && t <= 479)
+            txTransparentBlt (txDC (), 149,   540, CatX, CatY, Cat, CatX * (((t - 400) / 10) % 4),
+                              CatY * 5, RGB (255, 255, 255));
+
+        if (479 < t && t <= 509)
+            txTransparentBlt (txDC (), 149, 540, CatX, CatY, Cat, CatX * 2,
+                              CatY * 4, RGB (255, 255, 255));
+
+        if (509 < t && t <= 809)
+            txTransparentBlt (txDC (), 149 - (t - 509), 540, CatX, CatY, Cat,
+                              CatX * ((((t - 510) / 10) % 2) + 2), CatY * 3, RGB (255, 255, 255));
+
+        if (t == 810) t = 0;
+
+        t++;
+        txSleep (SleepTime);
+        }
+    txEnd ();
+    txPlaySound (NULL);
+    }
+
+void Menu ()
+    {
+    double CenterX = WindowWidth/2,
+           CenterY = WindowHeight/2;
+
+    double ButtonsX = txGetExtentX (Buttons) / 2,
+           ButtonsY = txGetExtentY (Buttons) / 3;
+
+    double MinXButtons  = CenterX - ButtonsX/2,
+           MaxXButtons  = CenterX + ButtonsX/2;
+
+    double MinYNewGame  = CenterY - ButtonsY*1.5,
+           MaxYNewGame  = CenterY - ButtonsY/2,
+           MaxYControl  = CenterY + ButtonsY/2,
+           MaxYExitGame = CenterY + ButtonsY*1.5;
+
+    MenuButtons (CenterX, CenterY, ButtonsX, ButtonsY, MinXButtons, MaxXButtons, MinYNewGame,
+                 MaxYNewGame, MaxYControl, MaxYExitGame);
+
+    txDeleteDC (MainMenu);
+    txDeleteDC (Buttons);
+    txDeleteDC (Background);
+    txDeleteDC (Cat);
+    }
+
+void Ball::MoveBall ()
+    {
+    txSetColor (RGB (223, 192, 110));
+    txSetFillColor (RGB (223, 192, 110));
+
+    txCircle (x1, y1, r);
+
+    x1 += vx * dt;
+    y1 -= vy * dt;
+
+    if (x1 > WindowWidth - 202  - r)
+        {
+        vx = - vx;
+        x1 = x1 - 2 * (x1 - WindowWidth + 202  + r);
+
+        txPlaySound ("Audio/Game/Wall.wav");
+        }
+
+    if (x1 < 0 + 202 + r)
+        {
+        vx = - vx;
+        x1 = x1 - 2 * (x1 - 202 - r);
+
+        txPlaySound ("Audio/Game/Wall.wav");
+        }
+
+    if (x1 <= x + 140 && x <= x1 && y - r <= y1 && y1 <= y + 8)
+        {
+        vy = - vy;
+        y1 = y  - r;
+        k+=dt-2;
+
+        txPlaySound ("Audio/Game/Point.wav");
+        }
+
+    if (y1 < 0 + r)
+        {
+        vy = - vy;
+        y1 = y1 - 2 * (y1 - r);
+
+        txPlaySound ("Audio/Game/Wall.wav");
+        }
+
+    if (txGetAsyncKeyState ('1'))
+        {
+        dt = 3;
+        r = 50;
+        }
+
+    if (txGetAsyncKeyState ('2'))
+        {
+        dt = 4;
+        r = 40;
+        }
+
+    if (txGetAsyncKeyState ('3'))
+        {
+        dt = 5;
+        r = 30;
+        }
+    }
+
+void Ball::Rectangle ()
+    {
+    txSetColor (RGB (223, 192, 110));
+    txSetFillColor (RGB (223, 192, 110));
+
+    txRectangle (x, y, x + 140, y + 40);
+
+    if (x + 140 > WindowWidth - 202) x = WindowWidth - 140 - 202;
+    if (x < 0 + 202) x = 0 + 202;
+
+    if (txGetAsyncKeyState (VK_RIGHT)) x += 30;
+    if (txGetAsyncKeyState (VK_LEFT))  x -= 30;
+    }
+
+void Ball::Score ()
+    {
+    txSetTextAlign (TA_CENTER);
+    txSelectFont ("Arial Black", 100);
+
+    char score [5] = "";
+    sprintf (score, "%d", k);
+
+    txSetColor (TX_WHITE);
+    txSetFillColor (TX_WHITE);
+    txRectangle (32, 210, 170, 285);
+
+    txSetColor (RGB (139, 109, 31));
+    txDrawText (32, 210, 170, 285, score);
+    }
+
+void GameOverText ()
+    {
+    txSetTextAlign (TA_CENTER);
+    txSelectFont ("Arial Black", 180);
+
+    int CenterX = WindowWidth/2,
+        CenterY = WindowHeight/2;
+
+    int textSizeX = txGetTextExtentX ("GAME OVER"),
+        textSizeY = txGetTextExtentY ("GAME OVER");
+
+    txSetColor (TX_RED);
+
+    txDrawText (CenterX - textSizeX/2, CenterY - textSizeY/2,
+                CenterX + textSizeX/2, CenterY + textSizeY/2, "GAME OVER");
+    }
+
+void MiniGame ()
+    {
+    Ball ball1 = {0, 600, 600, 200, 5, 3, 3, 50, 0};
+
+    txBegin ();
+    while (ball1.y1 <= WindowHeight + ball1.r)
+        {
+        if (txGetAsyncKeyState (VK_ESCAPE))
+            {
+            if (txMessageBox ("Если выйдешь из игры, то весь прогресс коту под хвост!") == IDOK)
+                {
+                break;
+                }
+            }
+
+        txSetFillColor (TX_WHITE);
+        txClear ();
+
+        txBitBlt  (txDC(), 0, 0, WindowWidth, WindowHeight, Score, 0, 0);
+
+        ball1.MoveBall ();
+        ball1.Rectangle ();
+        ball1.Score ();
+
+        txSleep (SleepTime);
+        }
+
+        txPlaySound ("Audio/Game/GameOver.wav");
+        int t = 0;
+        while (t <= 10)
+            {
+            txBitBlt  (txDC(), 0, 0, WindowWidth, WindowHeight, Background, 0, 0);
+            GameOverText ();
+            t++;
+            txSleep (SleepTime);
+            }
+            txSetFillColor (TX_BLACK);
+
+        double CenterX = WindowWidth/2,
+               CenterY = WindowHeight/2;
+
+        while (t<=12)
+            {
+            txTransparentBlt (txDC (), CenterX - 226, CenterY + 100, 0, 0,
+                               BackToMenu, 0, 0, RGB (255, 255, 255));
+            t++;
+            txSleep (SleepTime);
+            }
+        while (! txGetAsyncKeyState (VK_SPACE))
+            {
+            txRectangle (600, 350, 680, 370);
+            txBitBlt (txDC (), 0, 0, 0, 0, MainMenu, 0, 0);
+            }
+
+       txPlaySound ("Audio/MainMenu/Sound.wav");
+
+    txEnd ();
+    }
